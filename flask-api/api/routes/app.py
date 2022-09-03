c9ff293110861e1063db44ad01db2894a2a14fed
KX61bxFCws9ZxoBDRWos <jrbbrawner@gmail.com>
2022-09-02 19:47:05 -0500

    final - providers

--- flask-api/api/routes/app.py
+++ flask-api/api/routes/app.py
@@ -3,7 +3,7 @@ from .. import login_manager
 from flask_login import logout_user
 from sqlalchemy import create_engine, MetaData
 import json
-from flask import current_app as app
+from flask import current_app as app, jsonify
 
 app_bp = Blueprint('app_bp', __name__)
 
