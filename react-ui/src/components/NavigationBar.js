ba64da4bc8990ec64b9c22bfb02eea5b6c718275
jrbrawner <jrbbrawner@gmail.com>
2022-11-28 12:15:51 -0600

    changing for deployment

--- react-ui/src/components/NavigationBar.js
+++ react-ui/src/components/NavigationBar.js
@@ -54,16 +54,17 @@ export default function NavigationBar(props){
                   />
                 <Button variant="outline-success" type="submit" >Search</Button>
               </Form>
+              {/*}
               <Nav>
                 <Nav.Link href="/notifications">Notifications</Nav.Link>
               </Nav>
-              
+              {*/}
               <Nav>
                 <Nav.Link href="/role">Role Management</Nav.Link>
               </Nav>
               
               <NavDropdown title={<img src="person-circle.svg"/>}  >
-                <NavDropdown.Item><i className="fa fa-envelope fa-fw"></i>Profile</NavDropdown.Item>
+              <NavDropdown.Item href="/profile" eventKey='3'><i className="fa fa-sign-out fa-fw"></i>Profile</NavDropdown.Item>
                 <NavDropdown.Item><i className="fa fa-gear fa-fw"></i>Settings</NavDropdown.Item>
                 <NavDropdown.Item divider />
                 <NavDropdown.Item href="/logout" eventKey='3'><i className="fa fa-sign-out fa-fw"></i>Logout</NavDropdown.Item>
@@ -105,7 +106,7 @@ export default function NavigationBar(props){
           </Nav>
           
           <NavDropdown title={<img src="person-circle.svg"/>}>
-              <NavDropdown.Item href="/profile" eventkey='1'><i className="fa fa-envelope fa-fw"></i>Profile</NavDropdown.Item>
+              <NavDropdown.Item href="/profile" eventKey='3'><i className="fa fa-sign-out fa-fw"></i>Profile</NavDropdown.Item>
               <NavDropdown.Item><i className="fa fa-gear fa-fw"></i>Settings</NavDropdown.Item>
               <NavDropdown.Item divider />
               <NavDropdown.Item href="/logout" eventKey='3'><i className="fa fa-sign-out fa-fw"></i>Logout</NavDropdown.Item>
